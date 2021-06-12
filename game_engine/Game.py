7c5338c9de60b2df9c329b57d8f0db6d901a346a
Siddhartha <siddhart005@e.ntu.edu.sg>
2021-06-12 16:42:56 +0800

    added capturebot5000

--- game_engine/Game.py
+++ game_engine/Game.py
@@ -34,7 +34,7 @@ class StandardGame :
             player_pieces = self.board.get_pieces_by_color('white')
             opp_pieces = self.board.get_pieces_by_color('black')
             en_passant, castling, move = self.white_player.tick(\
-                    self.white_states, player_pieces, opp_pieces)
+                    self.white_states, player_pieces, opp_pieces, strategy = 'capturebot5000')
             self.black_states['en_passant'] = en_passant
             self.white_states['castling'] = castling
             self.to_move = 'black'
@@ -44,7 +44,7 @@ class StandardGame :
             player_pieces = self.board.get_pieces_by_color('black')
             opp_pieces = self.board.get_pieces_by_color('white')
             en_passant, castling, move = self.black_player.tick(\
-                    self.black_states, player_pieces, opp_pieces)
+                    self.black_states, player_pieces, opp_pieces, strategy = 'capturebot5000')
             self.white_states['en_passant'] = en_passant
             self.black_states['castling'] = castling
             self.to_move = 'white'
@@ -87,7 +87,7 @@ class StandardGame :
 if __name__ == "__main__" :
     game = StandardGame()
     makedirs('out_files', exist_ok = True)
-    game.print_game_state('black', out_file = 'out_files/move_000.jpg')
+    game.print_game_state('white', out_file = 'out_files/move_000.jpg')
     for i in range(1,201) :
         game.tick()
         game.print_game_state('white', out_file = str('out_files/move_%03d.jpg' % (i)))
