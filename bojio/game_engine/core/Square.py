e757a97592d3e313f62e1078875fe37e12f70b2a
Siddhartha <siddhart005@e.ntu.edu.sg>
2021-08-12 22:45:17 +0800

    square unit-tests complete

--- bojio/game_engine/core/Square.py
+++ bojio/game_engine/core/Square.py
@@ -1,39 +1,83 @@
+"""
+Contains class description of a square on the chessboard.
+"""
+
 from dataclasses import dataclass, field
 from bojio.game_engine.core.Piece import Piece, PieceColors, PieceNames
 
 
-@dataclass()
+@dataclass(init=True, repr=True, eq=False, order=False, unsafe_hash=False, frozen=False)
 class Square:
-    piece: Piece = field(default=Piece())
-    xy: tuple[int] = field(default=(0, 0))
-    square_id: str = "a1"
-    annotate: bool = False
+    """
+    Dataclass that defines a square of a chessboard. Each standard chess board
+    is made of 64 squares, and each square contains attributes about its
+    location, piece, ID, etc.
+
+    Squares should be compared by their xy coordinates, so we write our own
+    equality dunder method. There is no natural ordering between squares, so we
+    disable ordering.
+
+    Squares are not frozen, since the chess piece on the square can be changed.
+    """
+
+    _piece: Piece = field(default=Piece())
+    _xy: tuple[int] = field(default=(0, 0))
+    _square_id: str = "a1"
+    _annotate: bool = False
 
     def __post_init__(self) -> None:
-        self.annotate: bool = True if (self.xy[0] == 0 or self.xy[1] == 0) else False
-
-    def change_piece(self, new_piece: Piece) -> None:
-        self.piece = new_piece
-
-    def is_unoccupied(self) -> bool:
-        return self.piece == Piece()  # Piece() creates an "empty-piece"
+        """
+        Assigns _annotate attribute correctly based on the xy coordinates of
+        the square. The annotate property is helpful when printing the board,
+        since we only want to print the square_id on select squares to minimize
+        clutter.
+        """
+        self._annotate = True if (self._xy[0] == 0 or self._xy[1] == 0) else False
 
     def __str__(self) -> str:
+        """
+        Pretty-print the square, including when the square is empty.
+        """
         if self.is_unoccupied():
             return str(
-                "[%s,%s] EMPTY SQUARE" % (",".join(map(str, self.xy)), self.square_id)
+                "[%s,%s] EMPTY SQUARE" % (",".join(map(str, self._xy)), self._square_id)
             )
         return str(
             "[%s,%s] %s %s"
             % (
-                ",".join(map(str, self.xy)),
-                self.square_id,
-                self.piece.color.name,
-                self.piece.name.name,
+                ",".join(map(str, self._xy)),
+                self._square_id,
+                self._piece.color.name,
+                self._piece.name.name,
             )
         )
 
+    def __eq__(self, other) -> bool:
+        """
+        Equality check, makes sure other object is of the right type.
+        """
+        if not isinstance(other, Square):
+            raise TypeError("Cannot compare Square object to object of different type")
+
+        return self._xy == other._xy
+
     def print(self) -> str:
+        """
+        Less verbose print of piece currently on this square.
+        """
         if self.is_unoccupied():
             return ""
-        return str("%s-%s" % (self.piece.color.name, self.piece.name.name))
+        return str("%s-%s" % (self._piece.color.name, self._piece.name.name))
+
+    def change_piece(self, new_piece: Piece) -> None:
+        """
+        Changes the piece currently sitting on this square.
+        """
+        self._piece = new_piece
+
+    def is_unoccupied(self) -> bool:
+        """
+        Returns true if a non-empty chess piece is on this square, false
+        otherwise.
+        """
+        return self._piece == Piece()  # Piece() creates an "empty-piece"
---
+++ tests/test_Square.py
@@ -0,0 +1,94 @@
+"""
+Pytest unit tests for the Square dataclass.
+"""
+import pytest
+from bojio.game_engine.core.Square import Square
+from bojio.game_engine.core.Piece import Piece, PieceNames, PieceColors
+
+
+def test_square_init():
+    """
+    Tests basic square init.
+    """
+
+    sq = Square()
+    assert sq._piece == Piece()
+    assert sq._xy == (0, 0)
+    assert sq._square_id == "a1"
+    assert sq._annotate == True
+
+
+def test_square_annotate():
+    """
+    Only first row and first column should have annotate set to true.
+    """
+    for x, p in enumerate(["a", "b", "c", "d", "e", "f", "g", "h"]):
+        for y in range(1, 9):
+            sq = Square(Piece(PieceColors.BLACK, PieceNames.PAWN), (x, y), f"{p}{y}")
+            if x == 0 or y == 0:
+                assert sq._annotate
+            else:
+                assert not sq._annotate
+
+
+def test_square_equality():
+    """
+    Test square equality is only based off xy coords.
+    """
+
+    sq0 = Square(Piece(PieceColors.BLACK, PieceNames.PAWN), (0, 3), "a3")
+    sq1 = Square(Piece(PieceColors.WHITE, PieceNames.PAWN), (0, 3), "a4")
+    assert sq0 == sq1
+
+    sq2 = Square(Piece(PieceColors.BLACK, PieceNames.PAWN), (1, 3), "a3")
+    assert sq0 != sq2
+
+    # Test that comparing Square to another class object raises TypeError
+    with pytest.raises(TypeError) as exception_info:
+        _ = sq0 == Piece()
+    assert (
+        str(exception_info.value)
+        == "Cannot compare Square object to object of different type"
+    )
+
+
+def test_square_is_unoccupied():
+    """
+    Test that .is_unoccupied() method works.
+    """
+    sq0 = Square(Piece(PieceColors.EMPTY, PieceNames.EMPTY), (0, 3), "a3")
+    assert sq0.is_unoccupied()
+
+    sq1 = Square(Piece(PieceColors.BLACK, PieceNames.PAWN), (0, 3), "a3")
+    assert not sq1.is_unoccupied()
+
+
+def test_square_change_piece():
+    """
+    Test that .change_piece() method works.
+    """
+
+    sq = Square(Piece(PieceColors.EMPTY, PieceNames.EMPTY), (0, 3), "a3")
+
+    new_piece = Piece(PieceColors.BLACK, PieceNames.QUEEN)
+    sq.change_piece(new_piece)
+
+    assert sq._piece == new_piece
+
+
+def test_square_print():
+    """
+    Test that __str__() and .print() methods return expected strings.
+    """
+
+    sq = Square(Piece(PieceColors.EMPTY, PieceNames.EMPTY), (0, 3), "a3")
+
+    assert sq.print() == ""
+    assert str(sq) == "[%s,%s] EMPTY SQUARE" % (
+        ",".join(map(str, sq._xy)),
+        sq._square_id,
+    )
+
+    sq = Square(Piece(PieceColors.WHITE, PieceNames.KING), (0, 3), "a3")
+    assert sq.print() == "WHITE-KING"
+    assert str(sq) == "[0,3,a3] WHITE KING"
